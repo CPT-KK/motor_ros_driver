1c5f635f4fd1d738b6589e69494462c4c4504162
cptkkk <cptkk@qq.com>
2023-12-04 15:12:29 +0800

    fix div 0

--- include/USVMotor.h
+++ include/USVMotor.h
@@ -125,7 +125,9 @@ void USVMotor::torqeedo_setpoint_ros_to_can(const std_msgs::Int16::ConstPtr& msg
 
     if (raw_rpm > -30 && raw_rpm < 30) {
         ROS_WARN("Setpoint %d rpm for torqeedo 0x%X is too small to trigger an action.", raw_rpm, idx);
-        raw_rpm = raw_rpm / abs(raw_rpm) * 30;
+        if (raw_rpm != 0) {
+            raw_rpm = raw_rpm / abs(raw_rpm) * 30;
+        }  
     }
 
     // CAN frame payload for torqeedo
